5142723cb5227caac6e03f1bb13fcf4144e61ce7
IsmaelLG <ismael.llopart@gmail.com>
2022-10-18 12:14:32 +0200

    first commit

---
+++ src/main/java/s4/n1/exercise_1/S04t01n01LlopartGilIsmaelApplication.java
@@ -0,0 +1,13 @@
+package s4.n1.exercise_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04t01n01LlopartGilIsmaelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04t01n01LlopartGilIsmaelApplication.class, args);
+	}
+
+}
